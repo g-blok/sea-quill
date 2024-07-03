71ecab6ff9b0897160f13feb13f4752a9acb59ea
g-blok <gwblocher@gmail.com>
2024-07-03 09:28:25 -0700

    time handling in express app

--- backend/src/controllers/chartsController.ts
+++ backend/src/controllers/chartsController.ts
@@ -12,7 +12,7 @@ const getCharts = async (req: Request, res: Response) => {
     if (error) {
       throw error;
     }
-    console.log('charts: ', charts)
+
     res.json(charts);
   } catch (error) {
     console.error('Error fetching charts:', error);
@@ -22,15 +22,15 @@ const getCharts = async (req: Request, res: Response) => {
 
 const getChartData = async (req: Request, res: Response) => {
   const { id } = req.params;
+  const { startDate, endDate } = req.query;
 
   try {
     // Fetch the chart's SQL query using Supabase
     const { data: charts, error } = await supabase
       .from('charts')
-      .select('sql_query')
+      .select(`sql_query, date_field_field`)
       .eq('id', id);
 
-    console.log('charts data: ', charts)
     if (error) {
       throw error;
     }
@@ -40,12 +40,26 @@ const getChartData = async (req: Request, res: Response) => {
     }
 
     const chart = charts[0];
-    console.log('chart.sql_query: ', chart.sql_query)
-    // Execute the chart's SQL query using Supabase
+    let dataQuery = chart.sql_query;
+    if (chart.date_field_field && startDate && endDate) {
+      function addConditionToSQL(query: string, condition: string): string {
+        const splitQuery = query.split(/(group by)/i);
+        if (splitQuery.length < 3) {
+          return  `${query} ${timeCondition}`;
+        }
+        const modifiedQuery = `${splitQuery[0].trim()} ${condition} ${splitQuery[1]} ${splitQuery[2].trim()}`;
+        return modifiedQuery;
+      }     
+ 
+      const timeCondition = `WHERE ${chart.date_field_field} BETWEEN '${startDate}' AND '${endDate}'`;
+      dataQuery = addConditionToSQL(dataQuery, timeCondition);
+    } 
+    console.log('dataQuery: ', dataQuery)
+
     const { data: results, error: queryError } = await supabase.rpc('execute_sql', {
-      query: chart.sql_query,
+      query: dataQuery,
     });
-    console.log('results data: ', results)
+
     if (queryError) {
       throw queryError;
     }
