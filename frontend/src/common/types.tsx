a7dede2669df650d01feb46b9ab7e01b2e91b957
g-blok <gwblocher@gmail.com>
2024-07-03 09:29:37 -0700

    added time range options constant

---
+++ frontend/src/common/types.tsx
@@ -0,0 +1,6 @@
+export const TIME_RANGE_OPTIONS = [
+    { value: 'LAST_90_DAYS', label: 'Last 90 Days' },
+    { value: 'LAST_60_DAYS', label: 'Last 60 Days' },
+    { value: 'LAST_30_DAYS', label: 'Last 30 Days' },
+    { value: 'CURRENT_MONTH', label: 'Current Month' },
+];
