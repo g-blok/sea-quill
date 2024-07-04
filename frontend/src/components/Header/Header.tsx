3bc68fb251b9f982381d8a059ff8a8ff398379ac
g-blok <gwblocher@gmail.com>
2024-07-04 10:24:59 -0700

    removed mui from header, added RRRRRR button

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,21 +1,49 @@
 import React from 'react';
-import { AppBar, Toolbar, Button, Tabs, Tab } from '@mui/material';
-import { Link } from 'react-router-dom';
-import logo from '../../assets/sea-quill-logo.png';
+import { NavLink  } from 'react-router-dom';
+import logo from '../../assets/sea-quill-brand.png';
+import RRRRRR from '../RRRRRR/RRRRRR';
 
 const Header: React.FC = () => {
   return (
-    <div position="static" className="white">
-      <Toolbar className="flex justify-between">
-        <img src={logo} alt="Logo" className="h-16 mr-4 p-2" />
+    <div position="static" className="white mr-14">
+      <div className="flex justify-between">
+        <img src={logo} alt="Logo" className="h-20 mr-4 p-2" />
         <div className="flex-grow">
-          <Tabs value={0} className="flex justify-center">
-            <Tab label="Dashboards" component={Link} to="/dashboards" />
-            <Tab label="Charts" component={Link} to="/charts" />
-          </Tabs>
+          <nav className="flex justify-center gap-4 p-6 text-xl">
+            <NavLink
+              to="/dashboards"
+              className={({ isActive }) =>
+                `px-4 py-2 rounded-xl hover:opacity-60 ${
+                  isActive ? 'bg-secondary-light text-white' : 'text-black'
+                }`
+              }
+            >
+              dashboards
+            </NavLink>
+            <NavLink
+              to="/charts"
+              className={({ isActive }) =>
+                `px-4 py-2 rounded-xl hover:opacity-60 ${
+                  isActive ? 'bg-secondary-light text-white' : 'text-black'
+                }`
+              }
+            >
+              charts
+            </NavLink>
+            <NavLink
+              to="/data"
+              className={({ isActive }) =>
+                `px-4 py-2 rounded-xl hover:opacity-60 ${
+                  isActive ? 'bg-secondary-light text-white' : 'text-black'
+                }`
+              }
+            >
+              data
+            </NavLink>
+          </nav>
         </div>
-        <Button color="primary" variant="contained">RRRRRRR!</Button>
-      </Toolbar>
+        <RRRRRR />
+      </div>
     </div>
   );
 };
