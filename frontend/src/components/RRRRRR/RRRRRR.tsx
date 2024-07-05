4ac26ce187d5fdcfd05eb08f0333b65df697558e
g-blok <gwblocher@gmail.com>
2024-07-04 18:00:38 -0700

    improved pirate ship

--- frontend/src/components/RRRRRR/RRRRRR.tsx
+++ frontend/src/components/RRRRRR/RRRRRR.tsx
@@ -1,29 +1,37 @@
 import React, { useState } from 'react';
-import ship from '../../assets/ship.png';
+import SHIP from '../../assets/ship.png';
+
+interface Ship {
+    id: number;
+}
 
 const PirateButton: React.FC = () => {
-  const [animate, setAnimate] = useState(false);
+    const [ships, setShips] = useState<Ship[]>([]);
+    const [shipId, setShipId] = useState(0);
 
   const handleClick = () => {
-    setAnimate(true);
-    setTimeout(() => setAnimate(false), 300000);
+    setShips([...ships, { id: shipId }]);
+    setShipId(shipId + 1);
+    setTimeout(() => {
+        setShips((ships) => ships.filter((ship) => ship.id !== shipId));
+    }, 30000);
   };
 
   return (
-    <div className="flex flex-col items-center relative">
+    <div className="flex flex-col items-center relative py-6">
       <button
         className="bg-secondary-dark hover:bg-secondary text-white font-bold rounded-xl px-10 h-12"
         onClick={handleClick}
       >
         RRRRRR!
       </button>
-      {animate && (
+      {ships.map((ship) => (
         <img
-          src={ship}
+          src={SHIP}
           alt="Pirate Ship"
           className="fixed bottom-0 left-0 w-24 animate-sail"
         />
-      )}
+      ))}
     </div>
   );
 };
